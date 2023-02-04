f65351c1b13a79a7ff39d124bd377822a8887468
StaubRacing <staubracing@gmail.com>
2023-02-04 13:41:25 -0600

    added destructoring assignment to ex. 12

--- 2_Operators/12_exercise.js
+++ 2_Operators/12_exercise.js
@@ -1,10 +1,12 @@
 let a = 'red';
 let b = 'blue';
 
-let c = a;
-a = b;
-b = c;
+// let c = a;
+// a = b;
+// b = c;
+
+[a, b] = [b, a] // came back and used destructuring assignment to swap the varibles
 
 
-console.log(a);
-console.log(b);
+console.log(`This is 'a = red' but now is ${a}`);
+console.log(`This is 'b = blue' but now is ${b}`);
