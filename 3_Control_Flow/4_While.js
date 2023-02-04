acf90a820a927539daf8a529ec9d094b28b34e12
StaubRacing <staubracing@gmail.com>
2023-02-04 15:17:07 -0600

    Completed lessons 4 & 5 in control flow

---
+++ 3_Control_Flow/4_While.js
@@ -0,0 +1,14 @@
+// While Loops
+
+
+
+// while (condition) {
+//     Statment
+// }
+
+
+let i = 0
+while (i <= 5) {
+    if (i % 2 !== 0 ) console.log (i);
+    i++;
+}
---
+++ 3_Control_Flow/5_do_While.js
@@ -0,0 +1,26 @@
+// Do while:
+
+// for (let i = 5; i >= 1; i--) {
+//     if (i % 2 !== 0 )
+//         console.log (i); 
+//     }
+
+// let i = 0
+// while (i <= 5) {
+//     if (i % 2 !== 0 ) console.log (i);
+//     i++;
+// }
+
+
+// let i = 0;
+// do{
+//     if (i % 2 !== 0 ) console.log (i);
+//     i++;
+// } while( i <= 5);
+
+let i = 9;
+do{
+    if (i % 2 !== 0 ) console.log (i);
+    i++;
+} while( i <= 5);
+
