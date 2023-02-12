b157469981521b327c127218f32a0a3e0a67d749
StaubRacing <staubracing@gmail.com>
2023-02-12 09:52:36 -0600

    completed exercise 3

--- 3_Control_Flow/12_Exercise_3.js
+++ 3_Control_Flow/12_Exercise_3.js
@@ -6,8 +6,21 @@
 // Not Divisible by both 3 & 5 => input
 // Not  a number => 'Not a Number
 
-const output = fizzBuzz(3);
+const output = fizzBuzz(input); // replace input with the value i.e 5, 10, 15, true, false, 'Joe'
 console.log(output);
+
 function fizzBuzz(input){
+    if (typeof input !== 'number')
+        return Nan // <== this is cleaner
+        // return 'Not a Number'; other way of doing it
+    if ((input % 3 === 0) && (input % 5 === 0))
+        return 'FizzBuzz';
+
+    if (input % 3 == 0)
+        return 'Fizz';
+        
+    if (input % 5 == 0)
+        return 'Buzz';
 
 }
+
