36ca38560890e0f8203c135de252a11ae59795c1
StaubRacing <staubracing@gmail.com>
2023-02-12 09:24:59 -0600

    Add: file 12_Exercise_3.js

---
+++ 3_Control_Flow/12_Exercise_3.js
@@ -0,0 +1,13 @@
+// Exercise 12 - Fizz Buzz
+
+// Divisible by 3 => Fizz
+// Divisible by 5 => Buzz
+// Divisible by both 3 & 5 => FizzBuzz
+// Not Divisible by both 3 & 5 => input
+// Not  a number => 'Not a Number
+
+const output = fizzBuzz(3);
+console.log(output);
+function fizzBuzz(input){
+
+}
