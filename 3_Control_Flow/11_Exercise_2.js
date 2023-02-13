1fa8ab1679b2a1c0c8a30f683adbff10a6a505ba
staubracing <staubracing@gmail.com>
2023-02-13 15:57:25 -0600

    Add: Added ChatGpt soulations

--- 3_Control_Flow/11_Exercise_2.js
+++ 3_Control_Flow/11_Exercise_2.js
@@ -5,3 +5,20 @@ console.log(`The thing is ${isLandscape(500, 600)}`);
 function isLandscape (width, height) {
  return (width > height) ? true : false;
 }
+
+
+// ChatGpt
+
+// function isLandscape(width, height) {
+//     return width > height;
+//   }
+  
+//   const imageWidth = 800;
+//   const imageHeight = 600;
+  
+//   if (isLandscape(imageWidth, imageHeight)) {
+//     console.log("The image is a landscape image");
+//   } else {
+//     console.log("The image is not a landscape image");
+//   }
+  
