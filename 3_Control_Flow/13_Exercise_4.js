39cf4fa5b552dbd6902dd57ff0d695b64241cb34
StaubRacing <staubracing@gmail.com>
2023-02-15 06:09:08 -0600

    new file

---
+++ 3_Control_Flow/13_Exercise_4.js
@@ -0,0 +1,14 @@
+
+// Speedlimit = 70
+// 5 -> 1 point
+// math.floor(1.3)
+// 12 points -> suspended 
+
+function checkSpeed(speed) {
+    const Speedlimit = 70;
+
+    if (speed < Speedlimit)
+    console.log ('Ok');
+
+}
+
