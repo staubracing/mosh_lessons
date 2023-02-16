c110743b2c5c01f6862c310ddbbd18d8da4cd12f
StaubRacing <staubracing@gmail.com>
2023-02-16 05:59:47 -0600

    add: Added new exercise 4

--- 3_Control_Flow/13_Exercise_4.js
+++ 3_Control_Flow/13_Exercise_4.js
@@ -4,11 +4,38 @@
 // math.floor(1.3)
 // 12 points -> suspended 
 
+// checkSpeed (80);
+
+// function checkSpeed(speed) {
+//     const Speedlimit = 70;
+//     const kmPerPoint = 5;
+
+//     if (speed < Speedlimit + kmPerPoint)
+//     console.log ('Ok');
+//     else {
+//         const points = Math.floor((speed - Speedlimit) / kmPerPoint);
+//         if (points >= 12)
+//         console.log('License suspended')
+//         else
+//         console.log('Points', points);
+//     }
+// }
+
+checkSpeed (76);
 function checkSpeed(speed) {
     const Speedlimit = 70;
+    const kmPerPoint = 5;
 
-    if (speed < Speedlimit)
+    if (speed < Speedlimit + kmPerPoint){
     console.log ('Ok');
+    return;
+    }
 
+    const points = Math.floor((speed - Speedlimit) / kmPerPoint);
+    if (points >= 12)
+    console.log('License suspended')
+    else
+    console.log('Points', points);
 }
+
 
