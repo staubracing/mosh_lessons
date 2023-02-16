7bd549df050c453e44af00b8a11e33e78e702cfb
StaubRacing <staubracing@gmail.com>
2023-02-16 06:04:47 -0600

    added exercise 5

---
+++ 3_Control_Flow/14_Exercise_5.js
@@ -0,0 +1,9 @@
+// Exercise 5 even and off numbers
+
+showNUmbers (10);
+
+function showNUmbers (limit) {
+    // for loop up to limit
+    // irrette through and use moduluos
+    // console.log the even and odd based on if else statements
+}
