e9562e44c1007b8ea1af52f572a65a1b3da90a60
StaubRacing <staubracing@gmail.com>
2023-02-17 05:31:02 -0600

    added files

--- 3_Control_Flow/14_Exercise_5.js
+++ 3_Control_Flow/14_Exercise_5.js
@@ -6,4 +6,8 @@ function showNUmbers (limit) {
     // for loop up to limit
     // irrette through and use moduluos
     // console.log the even and odd based on if else statements
+    for (let i = 0; i <= limit; i++) { // i = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        if (i % 2 === 0) console.log(i, 'EVEN'); // 0, 2, 4, 6, 8, 10
+        else console.log(i, 'ODD'); // 1, 3, 5, 7, 9
+    }
 }
---
+++ 3_Control_Flow/15_Exercise_6.js
@@ -0,0 +1,6 @@
+// Exercise 6 Count Truthy
+// write a function called countTruthy that takes an array of values and returns the number of truthy values in the array
+// truthy values are values that resolve to true in a boolean context
+// ex: 1, true, 'hello', 34, -5, 'false' are all truthy values
+// ex: 0, false, '', null, undefined, NaN are all falsy values
+// ex: countTruthy([0, null, undefined, '', 2, 3]) // 2
