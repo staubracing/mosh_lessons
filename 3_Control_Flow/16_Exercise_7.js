52cfdbd292aba5f1ea3661f5a197708803bcd8f9
StaubRacing <staubracing@gmail.com>
2023-02-20 06:00:56 -0600

    Added: exercises 16 & 17

--- 3_Control_Flow/16_Exercise_7.js
+++ 3_Control_Flow/16_Exercise_7.js
@@ -11,6 +11,6 @@ showProperties(movie);
 
 function showProperties(obj) {
     for (let key in obj)
-        if (typeof obj[key] === 'string')
+        if (typeof obj[key] === 'number')
             console.log(key, obj[key]);
 }
---
+++ 3_Control_Flow/17_Exercise_8.js
@@ -0,0 +1,16 @@
+// exercise 8
+
+console.log(sum(10));
+
+// mulitple  of 3: 3, 6, 9
+// multiple of 5: 5, 10
+
+function sum(limit) {
+    let sum = 0;
+
+    for (let i = 0; i <= limit; i++) 
+        if (i % 3 === 0 || i % 5 === 0)
+            sum += i;    
+
+    return sum;   
+}
