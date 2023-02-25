7edfff254b42f5359140b4d93010ba0e740969b8
StaubRacing <staubracing@gmail.com>
2023-02-25 12:42:25 -0600

    Add:4-Objects lessons

---
+++ 4_Objects/1_basics.js
@@ -0,0 +1,16 @@
+// Basics
+
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function() {
+        console.log('draw');
+    }   
+};
+
+circle.draw(); // draw method  when a function is a property of an object it is called a method
+
