63c22617efcd9f6d69f6ed5e02684d4be6019a09
StaubRacing <staubracing@gmail.com>
2023-03-02 05:47:57 -0600

    two lessons added

--- 4_Objects/3_Constructor_Functions.js
+++ 4_Objects/3_Constructor_Functions.js
@@ -1,1 +1,12 @@
-// left off here
+// contructor function is a function that is used to create objects
+
+function Circle (radius) { // Pascal Notation first letter of the function is capital
+    this.radius = radius; // 'this' is a reference to the object that is executing this code
+    this.draw = function() { // this is a method
+        console.log('draw');
+    }
+}
+
+const circle = new Circle(1); // 'new' is an operator that creates an empty object and then calls the constructor function
+const x = {}; // this is the same as 'new Object()'
+
--- 4_Objects/4_Dynamic_Nature.js
+++ 4_Objects/4_Dynamic_Nature.js
@@ -1,1 +1,16 @@
-  
+ // Dynamic Nature
+ 
+    // This script is used to create a dynamic nature object.
+
+const circle = {
+    radius: 1
+};
+
+circle.color = 'yellow'; // this is a dynamic nature of an object
+circle.draw = function() {} // this is a dynamic nature of an object
+
+delete circle.color; // this is a dynamic nature of an object
+delete circle.draw; // this is a dynamic nature of an object
+
+console.log(circle); // this is a dynamic nature of an object
+
